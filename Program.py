e09500c78ebeaff2ef42c9d90ba1ed8bd6f50e8d
Alejandro Puente-Castro <apte3@hotmail.com>
2024-10-08 19:36:50 +0200

    Added Keras model loader and tunner

--- Program.py
+++ Program.py
@@ -1,4 +1,5 @@
 import Config
+from KerasLoader import KerasLoader
 from Loader import Loader
 
 
@@ -13,6 +14,14 @@ def main():
                                                  prob_flip_vertical=Config.PROB_FLIP_VERTICAL,
                                                  prob_blur=Config.PROB_BLUR,
                                                  blur_size=Config.BLUR_SIZE)
+
+    kerasLoader = KerasLoader()
+    model = kerasLoader.load_freeze_and_modify_model(name="ResNet50", num_layers_to_remove=1) # 23,587,712
+    model.summary()
+
+    #from keras.applications import ResNet50
+    #resnet50_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    #resnet50_model.summary()
 
     print(f"Total Loaded Images: {len(X)}")
     print(f"Total Loaded Labels: {len(Y)}")
